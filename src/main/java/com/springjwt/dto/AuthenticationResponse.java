dbc3bfb2ff1272d6a448674f991370ad6309c79f
Aymendhibi <98618828+Aymendhibi@users.noreply.github.com>
2023-10-19 19:05:54 +0100

    Update AuthenticationResponse.java

--- src/main/java/com/springjwt/dto/AuthenticationResponse.java
+++ src/main/java/com/springjwt/dto/AuthenticationResponse.java
@@ -2,4 +2,37 @@ package com.springjwt.dto;
 
 public record AuthenticationResponse(String jwtToken) {
 
+
+  private String jwtToken;
+
+    public AuthenticationResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+}
+
+package com.springjwt.dto;
+
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
