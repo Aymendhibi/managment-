e9b4fe4dd487b7787d3e397e36396d9c0019262c
Aymendhibi <98618828+Aymendhibi@users.noreply.github.com>
2023-10-19 19:11:17 +0100

    Update HelloResponse.java

--- src/main/java/com/springjwt/dto/HelloResponse.java
+++ src/main/java/com/springjwt/dto/HelloResponse.java
@@ -1,4 +1,17 @@
 package com.springjwt.dto;
 
-public record HelloResponse(String message) {
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
